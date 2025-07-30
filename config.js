8a29e223d147c4fdbc9cb58bdc4990bb5ce70f2b
darrelllynnetienne-code <darrelllynnetienne@gmail.com>
2025-07-29 20:16:28 -0400

    Darrell2.0 config.js

--- config.js
+++ config.js
@@ -15,14 +15,14 @@ module.exports = {
   
     SESSION_ID: process.env.SESSION_ID || "MEGALODON-MD~",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
-    BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
+    BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON~MD~OxgwXYpQ#03u0ULepQzpuYwAan76OGOhGYaZpPY3xkkJZOiCkY0Y",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     // ===== OWNER & DEVELOPER SETTINGS =====
     OWNER_NUMBER: process.env.OWNER_NUMBER || "50948702213",  // Owner's WhatsApp number
     
     OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "",           // Owner's name
     
-    DEV: process.env.DEV || "50948336180",                     // Developer's contact number
+    DEV: process.env.DEV || "50943184305",                     // Developer's contact number
     
     // ===== AUTO-RESPONSE SETTINGS =====
     
